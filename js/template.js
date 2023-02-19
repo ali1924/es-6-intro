ff67f77ae45c88086d267bb0d1d7d3b62f286843
ali1924 <mohammadali229987@gmail.com>
2023-02-19 22:46:20 +0600

    Function default parameter for not provided values

---
+++ js/template.js
@@ -0,0 +1,13 @@
+const singleQuate = 'Mohammad Ali';
+const doubleQuate = "Mohammad Ali";
+// Multiline 
+const backTick = `Mohammad 
+Ali
+dgqjhgdqw
+adfhqfwkqeefh
+dchasdga`;
+console.log(backTick);
+const a = 50;
+const b = 60;
+const muliString = `${a} ${a + b}`;
+console.log(muliString);
